7ca9afb26e1e6ec8933fd77689e88f22bc9e5786
Miquel Delgado <Mikiodelg@gmail.com>
2014-06-19 12:44:26 +0200

    Android primer commit

---
+++ gvent-android/src/edu/upc/eetac/dsa/dsaqp1314g5/gvent/android/api/Event.java
@@ -0,0 +1,103 @@
+package edu.upc.eetac.dsa.dsaqp1314g5.gvent.android.api;
+
+import java.sql.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Event {
+	
+	private Map<String, Link> links = new HashMap<String, Link>();
+	private int id;
+	private String title;
+	private String coordX;
+	private String coordY;
+	private String category;
+	private String description;
+	private String owner;
+	private String state;
+	private boolean publicEvent;
+	private long creationDate;
+	private Date eventDate;
+	private int popularity;
+	
+	public Map<String, Link> getLinks() {
+		return links;
+	}
+	public void setLinks(Map<String, Link> links) {
+		this.links = links;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getCoordX() {
+		return coordX;
+	}
+	public void setCoordX(String coordX) {
+		this.coordX = coordX;
+	}
+	public String getCoordY() {
+		return coordY;
+	}
+	public void setCoordY(String coordY) {
+		this.coordY = coordY;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getOwner() {
+		return owner;
+	}
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public boolean isPublicEvent() {
+		return publicEvent;
+	}
+	public void setPublicEvent(boolean publicEvent) {
+		this.publicEvent = publicEvent;
+	}
+	public long getCreationDate() {
+		return creationDate;
+	}
+	public void setCreationDate(long creationDate) {
+		this.creationDate = creationDate;
+	}
+	public Date getEventDate() {
+		return eventDate;
+	}
+	public void setEventDate(Date eventDate) {
+		this.eventDate = eventDate;
+	}
+	public int getPopularity() {
+		return popularity;
+	}
+	public void setPopularity(int popularity) {
+		this.popularity = popularity;
+	}
+
+}
